98695f051ee36dc45b0c5b662be1a89cb7ce1123
stevemann2705 <stevemann2705@gmail.com>
2018-10-18 16:00:05 +0530

    Initial Commit

---
+++ app/src/main/java/in/stevemann/sams/MainActivity.java
@@ -0,0 +1,19 @@
+package in.stevemann.sams;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/in/stevemann/sams/SignupActivity.java
@@ -0,0 +1,261 @@
+package in.stevemann.sams;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    RESTClient client = new RESTClient();
+
+    @BindView(R.id.input_firstname)
+    EditText _firstNameText;
+    @BindView(R.id.input_lastname)
+    EditText _lastNameText;
+    @BindView(R.id.input_email)
+    EditText _emailText;
+    @BindView(R.id.input_department)
+    Spinner _department;
+    @BindView(R.id.input_code)
+    EditText _code;
+    @BindView(R.id.input_password)
+    EditText _passwordText;
+    @BindView(R.id.input_reEnterPassword)
+    EditText _reEnterPasswordText;
+    @BindView(R.id.btn_signup)
+    Button _signupButton;
+    @BindView(R.id.link_login)
+    TextView _loginLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+                if (position == getCount()) {
+                    ((TextView) v.findViewById(android.R.id.text1)).setText("");
+                    ((TextView) v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+                }
+
+                return v;
+            }
+
+            @Override
+            public int getCount() {
+                return super.getCount() - 1; // you dont display last item. It is used as hint.
+            }
+
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add("computerscience");
+        adapter.add("education");
+        adapter.add("management");
+        adapter.add("Department"); //Spinner selection text
+
+        _department.setAdapter(adapter);
+        _department.setSelection(adapter.getCount()); //set the hint the default selection so it appears on launch.
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String firstName = _firstNameText.getText().toString();
+        String lastName = _lastNameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String department = _department.getSelectedItem().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+        String code = _code.getText().toString();
+
+        // TODO: Recheck Signup Logic
+        RequestParams params = new RequestParams();
+        params.put("firstName", firstName);
+        params.put("lastName", lastName);
+        params.put("department", department);
+        params.put("email", email);
+        params.put("password", password);
+        params.put("code", code);
+
+        client.post("users/add", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject timeline) {
+                boolean response = false;
+                String token = null;
+                try {
+                    response = (boolean) timeline.get("bool");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if (response) {
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    // On complete call either onLoginSuccess or onLoginFailed
+                                    onSignupSuccess();
+                                    // onLoginFailed();
+                                    progressDialog.dismiss();
+                                }
+                            }, 3000);
+                } else {
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    // On complete call either onLoginSuccess or onLoginFailed
+                                    //onLoginSuccess();
+                                    onSignupFailed();
+                                    progressDialog.dismiss();
+                                }
+                            }, 3000);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e("LOGIN AUTH RESPONSE", "Communication with server failed. Try again.");
+                // TODO: Handle onFailure() for signup
+            }
+        });
+
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Signup failed", Toast.LENGTH_LONG).show();
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String firstName = _firstNameText.getText().toString();
+        String lastName = _lastNameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String department = _department.getSelectedItem().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+        String code = _code.getText().toString();
+
+        if (firstName.isEmpty() || firstName.length() < 3) {
+            _firstNameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _firstNameText.setError(null);
+        }
+
+        if (lastName.isEmpty() || lastName.length() < 3) {
+            _lastNameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _lastNameText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if ("Department".equals(department)) {
+            TextView errorText = (TextView) _department.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);//just to highlight that this is an error
+            errorText.setText("Please select department");
+            valid = false;
+        } else {
+            System.out.println("No Error in Department Spinner");
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password Do not match");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        if (code.isEmpty() || !("code".equals(code))) {
+            _code.setError("Invalid Code");
+            valid = false;
+        } else {
+            _code.setError(null);
+        }
+
+        return valid;
+    }
+}
