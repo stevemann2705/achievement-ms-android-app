78385bc58311dc88e7616da89f10c0dd0e0aefbc
stevemann2705 <stevemann2705@gmail.com>
2018-11-12 16:35:52 +0530

    REMOVED: Unused Post Method

--- app/src/main/java/in/stevemann/sams/utils/RESTClient.java
+++ app/src/main/java/in/stevemann/sams/utils/RESTClient.java
@@ -21,10 +21,6 @@ public class RESTClient {
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static void post(Context context, String url, HttpEntity entity, String type, AsyncHttpResponseHandler responseHandler) {
-        client.post(context, getAbsoluteUrl(url), entity, type, responseHandler);
-    }
-
     private static String getAbsoluteUrl(String relativeUrl) {
         return BASE_URL + relativeUrl;
     }
