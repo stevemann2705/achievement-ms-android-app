48a0228137befd57fe5214b06e6bcbe184fd5b54
stevemann2705 <stevemann2705@gmail.com>
2018-11-18 15:13:45 +0530

    CHANGED: Password validation to be between 6 and 20 characters

--- app/src/main/java/in/stevemann/sams/LoginActivity.java
+++ app/src/main/java/in/stevemann/sams/LoginActivity.java
@@ -186,8 +186,8 @@ public class LoginActivity extends AppCompatActivity {
             _emailText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 6 || password.length() > 20) {
+            _passwordText.setError("between 6 and 20 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
--- app/src/main/java/in/stevemann/sams/SignupActivity.java
+++ app/src/main/java/in/stevemann/sams/SignupActivity.java
@@ -285,15 +285,15 @@ public class SignupActivity extends AppCompatActivity {
             System.out.println("No Error in Shift Spinner");
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 6 || password.length() > 20) {
+            _passwordText.setError("between 6 and 20 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
         }
 
-        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
-            _reEnterPasswordText.setError("Password Do not match");
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 6 || reEnterPassword.length() > 20 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password don't match");
             valid = false;
         } else {
             _reEnterPasswordText.setError(null);
