2f1489a143d07cc70de64add875ead107bdbaf4e
Steve Mann <stevemann2705@gmail.com>
2019-04-17 03:13:47 +0530

    ADDED: Loading achievement data of logged in teacher

--- app/src/main/java/in/stevemann/sams/tabs/MyTeacherAchievementsTab.java
+++ app/src/main/java/in/stevemann/sams/tabs/MyTeacherAchievementsTab.java
@@ -1,24 +1,37 @@
 package in.stevemann.sams.tabs;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import in.stevemann.sams.R;
-import in.stevemann.sams.models.AchievementModel;
+import in.stevemann.sams.adapters.AllTeacherAchievementsAdapter;
+import in.stevemann.sams.models.TeacherAchievementModel;
+import in.stevemann.sams.models.UserModel;
+import in.stevemann.sams.utils.RESTClient;
 
 public class MyTeacherAchievementsTab extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
-    private List<AchievementModel> achievementModels;
+    private List<TeacherAchievementModel> achievementModels;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,63 +49,54 @@ public class MyTeacherAchievementsTab extends Fragment {
 
     private void loadRecyclerViewData() {
 
-        //TODO: load data for my teacher achievements
-//        final ProgressDialog progressDialog = new ProgressDialog(getContext(),
-//                R.style.AppTheme_Light_Dialog);
-//        progressDialog.setIndeterminate(true);
-//        progressDialog.setMessage("Loading Approved Data...");
-//        progressDialog.show();
-//
-//        RequestParams params = new RequestParams();
-//
-//        RESTClient.get("achievements/all", params, new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
-//                for(int i = 0; i<timeline.length(); i++){
-//                    JSONObject o = null;
-//                    AchievementModel item = null;
-//                    try {
-//                        o = timeline.getJSONObject(i);
-//                        item = new AchievementModel(
-//                                o.getString("_id"),
-//                                o.getString("eventName"),
-//                                o.getString("rollNo"),
-//                                o.getInt("semester"),
-//                                o.getString("sessionFrom"),
-//                                o.getString("name"),
-//                                o.getBoolean("participated"),
-//                                o.getString("description"),
-//                                o.getString("shift"),
-//                                o.getString("sessionTo"),
-//                                o.getString("section"),
-//                                o.getString("department"),
-//                                o.getString("date"),
-//                                o.getString("rating"),
-//                                o.getString("approvedBy"),
-//                                o.getString("category"),
-//                                o.getString("title"),
-//                                o.getString("imageUrl"),
-//                                o.getBoolean("approved"),
-//                                o.getString("venue")
-//                        );
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//                    achievementModels.add(item);
-//                }
-//
-//                progressDialog.dismiss();
-//                adapter = new ApprovedAchievementsAdapter(achievementModels, getContext());
-//                recyclerView.setAdapter(adapter);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse){
-//                progressDialog.dismiss();
-//                Log.d("Failed: ", "" + statusCode);
-//                Log.d("Error : ", "" + throwable);
-//                Log.d("Caused By : ", "" + throwable.getCause());
-//            }
-//        });
+        final ProgressDialog progressDialog = new ProgressDialog(getContext(),
+                R.style.AppTheme_Light_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Loading All Teacher Achievement Data...");
+        progressDialog.show();
+
+        RequestParams params = new RequestParams();
+        params.put("userId", UserModel.getUserId());
+
+        RESTClient.get("tachievements/allUserid", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                for (int i = 0; i < timeline.length(); i++) {
+                    JSONObject o = null;
+                    TeacherAchievementModel item = null;
+                    try {
+                        o = timeline.getJSONObject(i);
+                        item = new TeacherAchievementModel(
+                                o.getString("_id"),
+                                o.getString("taType"),
+                                o.getBoolean("international"),
+                                o.getString("topic"),
+                                o.getString("published"),
+                                o.getBoolean("sponsored"),
+                                o.getBoolean("reviewed"),
+                                o.getString("date"),
+                                o.getString("description"),
+                                o.getBoolean("msi"),
+                                o.getString("place")
+                        );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    achievementModels.add(item);
+                }
+
+                progressDialog.dismiss();
+                adapter = new AllTeacherAchievementsAdapter(achievementModels, getContext());
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                progressDialog.dismiss();
+                Log.d("Failed: ", "" + statusCode);
+                Log.d("Error : ", "" + throwable);
+                Log.d("Caused By : ", "" + throwable.getCause());
+            }
+        });
     }
 }
